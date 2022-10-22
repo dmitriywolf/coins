89c439a446dbacaa77e2bebfbae4850994ebb861
Dmitriy Volchenko <dmitriyvolchenko3003@gmail.com>
2022-10-22 16:02:15 +0300

    initial commit

---
+++ pages/coins/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+
+export default function Coins() {
+  return (
+    <div>
+      <Head>
+        <title>Coins Table</title>
+        <meta name="description" content="Coins table" />
+      </Head>
+
+      <main>
+        <p>Coins table</p>
+      </main>
+
+
+    </div>
+  )
+}
